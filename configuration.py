9d760a19e463c1c25c594b4b78a16523dd9d272e
yang meng <62495705+S1mpleyang@users.noreply.github.com>
2022-12-15 14:05:21 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,13 +1,6 @@
 def build_config(dataset):
     cfg = type('', (), {})()
-    if dataset == 'TinyVirat':
-        cfg.data_folder = 'datasets/TinyVIRAT-v2-drop2/videos'
-        cfg.train_annotations = 'datasets/TinyVIRAT-v2-drop2/tiny_train_v2.json'
-        cfg.val_annotations = 'datasets/TinyVIRAT-v2-drop2/tiny_val_v2.json'
-        cfg.test_annotations = 'datasets/TinyVIRAT-v2-drop2/tiny_test_v2_public.json'
-        cfg.class_map = 'datasets/TinyVIRAT-v2-drop2/class_map.json'
-        cfg.num_classes = 26
-    elif dataset == 'ucf101':
+    if dataset == 'ucf101':
         cfg.data_folder = r"D:\DATASET\ucf101\UCF-101"
         cfg.train_split = r"D:\DATASET\ucf101\ucf101_train_split_1_videos.txt"
         cfg.val_split = r"D:\DATASET\ucf101\ucf101_val_split_1_videos.txt"
@@ -21,11 +14,10 @@ def build_config(dataset):
         cfg.dataset_path = r"D:\DATASET\HMDB51"
         cfg.num_classes = 51
     elif dataset == "myaction":
-        cfg.data_folder = r"D:\DATASET\myaction\ConstructionSafety"
-        cfg.train_split = r"D:\DATASET\myaction\train.txt"
+        cfg.data_folder = r"D:\DATASET\myaction\ConstructionSafety"    # location to CMA dataset
+        cfg.train_split = r"D:\DATASET\myaction\train.txt"             # location to train split
         cfg.val_split = r"D:\DATASET\myaction\test.txt"
-        #cfg.test_split = r"D:\DATASET\myaction\test_class.txt"
-        cfg.test_split = r"D:\DATASET\myaction\test.txt"
+        cfg.test_split = r"D:\DATASET\myaction\test.txt"               # location to test split
         cfg.dataset_path = r"D:\DATASET\myaction"
         cfg.num_classes = 7
     cfg.saved_models_dir = './results/saved_models'
