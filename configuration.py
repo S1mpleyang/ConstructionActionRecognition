9660800635cf01e7622cb6ea595244e103716e4f
yang meng <62495705+S1mpleyang@users.noreply.github.com>
2023-02-13 15:04:35 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,11 +1,11 @@
 def build_config(dataset):
     cfg = type('', (), {})()
     if dataset == "myaction":
-        cfg.data_folder = r"D:\DATASET\myaction\ConstructionSafety"    # location to CMA dataset
-        cfg.train_split = r"D:\DATASET\myaction\train.txt"             # location to train split
-        cfg.val_split = r"D:\DATASET\myaction\test.txt"
-        cfg.test_split = r"D:\DATASET\myaction\test.txt"               # location to test split
-        cfg.dataset_path = r"D:\DATASET\myaction"
+        cfg.data_folder = r"D:\DATASET\dataset\myaction"    # location to CMA dataset
+        cfg.train_split = r"D:\DATASET\dataset\train.txt"             # location to train split
+        cfg.val_split = r"D:\DATASET\dataset\test.txt"
+        cfg.test_split = r"D:\DATASET\dataset\test.txt"               # location to test split
+        cfg.dataset_path = r"D:\DATASET\dataset"
         cfg.num_classes = 7
     cfg.saved_models_dir = './results/saved_models'
     cfg.tf_logs_dir = './results/logs'
