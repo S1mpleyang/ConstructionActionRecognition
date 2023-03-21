b277eac721f92fcb3b61fda8a0b97017c361f0ae
yang meng <62495705+S1mpleyang@users.noreply.github.com>
2023-03-21 17:04:53 +0800

    Update roc_plot.py

--- draw/roc_plot.py
+++ draw/roc_plot.py
@@ -28,10 +28,10 @@ def plot_roc(
         linewidth=2,
 ):
     y_test = torch.load(label)
-    y_test = y_test.numpy()[1:, ].astype(np.int32)
+    y_test = y_test.numpy().astype(np.int32)
     try:
         y_score = torch.load(predict)
-        y_score = y_score.numpy()[1:, ].astype(np.float64)
+        y_score = y_score.numpy().astype(np.float64)
     except:
         y_score = np.load(predict)
         y_score = y_score[0:, ].astype(np.float64)
@@ -72,113 +72,6 @@ def plot_roc(
              label='{0} (AUC = {1:.3f})'.format(model_name, roc_auc["macro"]),
              color=color, linestyle=linestyle, linewidth=linewidth)
 
-
-##TSN
-plot_roc(
-    model_name="TSN",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet_224_8_0509_newdataset\resnet_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\tsn_out.npy",
-    color="blue",
-)
-
-##TSM
-plot_roc(
-    model_name="TSM",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet_224_8_0509_newdataset\resnet_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\tsm_out.npy",
-    color="darkorange",
-)
-
-##SlowFast
-plot_roc(
-    model_name="SlowFast",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet_224_8_0509_newdataset\resnet_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\slowfast_out.npy",
-    color="yellowgreen",
-)
-
-##R3D
-plot_roc(
-    model_name="R3D",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet_224_8_0509_newdataset\resnet_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet_224_8_0509_newdataset\resnet_tensor.pth",
-    color="gray",
-)
-
-##R(2+1)D
-plot_roc(
-    model_name="R(2+1)D",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet2p1d_224_8_0509_newdataset\resnet2p1d_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\resnet2p1d_224_8_0509_newdataset\resnet2p1d_tensor.pth",
-    color="purple",
-)
-
-##TEA
-plot_roc(
-    model_name="TEA",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\tea_224_8_0530\label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\tea_224_8_0530\tensor.pth",
-    color="cyan",
-)
-
-
-##ViT-B
-plot_roc(
-    model_name="ViT-B",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\ViT\experiments_myaction_8_vitbase_0807\eval\vitbase_224_8_0807\vitbase_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\ViT\experiments_myaction_8_vitbase_0807\eval\vitbase_224_8_0807\vitbase_tensor.pth",
-    color="g",
-)
-
-##ViT-L
-plot_roc(
-    model_name="ViT-L",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\ViT\experiments_myaction_8_vitlarge_0807\eval\vitlarge_224_8_0807\vitlarge_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\ViT\experiments_myaction_8_vitlarge_0807\eval\vitlarge_224_8_0807\vitlarge_tensor.pth",
-    color="g",
-    linestyle="--"
-)
-
-##SwinV1-B
-plot_roc(
-    model_name="SwinV1-B",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\SwinTransformer_AT_224_8_0530\SwinTransformer_AT_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp0531\experiments_myaction_8frame_results_0530\experiments_each_class\SwinTransformer_AT_224_8_0530\SwinTransformer_AT_tensor.pth",
-    color="red",
-)
-
-##SwinV1-L
-plot_roc(
-    model_name="SwinV1-L",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\swinViT\eval\swinVlarge_224_8_0809\swinVlarge_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\swinViT\eval\swinVlarge_224_8_0809\swinVlarge_tensor.pth",
-    color="red",
-    linestyle="--"
-)
-
-# ##Video Swin
-# plot_roc(
-#     model_name="Video Swin",
-#     label= r"D:\yangmeng_workspace\STR-Transformer\exp_0808\videoswin\eval\videoswin_224_8_0807\videoswin_label.pth",
-#     predict=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\videoswin\eval\videoswin_224_8_0807\videoswin_tensor.pth",
-#     color="sienna",
-# )
-
-##MViT
-plot_roc(
-    model_name="MViT",
-    label= r"D:\yangmeng_workspace\STR-Transformer\exp_0808\MViT\eval\mvitbase_224_8_0807\mvitbase_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\MViT\eval\mvitbase_224_8_0807\mvitbase_tensor.pth",
-    color="sienna",
-)
-
-plot_roc(
-    model_name="TimeSformer",
-    label=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\Timesformer-B-16\eval\timesformer_224_8_0811\timesformer_label.pth",
-    predict=r"D:\yangmeng_workspace\STR-Transformer\exp_0808\Timesformer-B-16\eval\timesformer_224_8_0811\timesformer_tensor.pth",
-    color="m",
-    linestyle="-."
-)
 
 ##ours
 plot_roc(
